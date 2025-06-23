8561bf039ad4f3bd5227b18a03074a097e22fbab
Timothy Li <timothy93li@gmail.com>
2025-06-23 16:12:21 +0100

    refactor: HomeScreen showinstruction state removed, no longer used

--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -1,11 +1,7 @@
-import React, { useState } from "react";
-
 export default function HomeScreen({ onStart, childName }) {
-  const [showInstructions, setShowInstructions] = useState(false);
-
   return (
     <div className="home-screen">
-      <p>Hi {childName}, press Start when you are ready!</p>
+      <p>Hello {childName}, press Start when you are ready.</p>
       <div style={{ marginTop: "20px" }}>
         <button
           onClick={onStart}
