3c14467b0f19fc7e341401c48c64f157dced01e7
Timothy Li <timothy93li@gmail.com>
2025-06-23 16:47:32 +0100

    feat: parent controls hidden under custom modal

--- src/components/StarChart/StarChartScreen.jsx
+++ src/components/StarChart/StarChartScreen.jsx
@@ -1,32 +1,54 @@
 import StarDisplay from "./StarDisplay";
+import { useState } from "react";
 
 export default function StarChartScreen({ child, manualUpdateStars }) {
+  const [showParentControls, setShowParentControls] = useState(false);
   return (
     <>
       <h2>{child.name}'s Star Chart</h2>
       <StarDisplay stars={child.stars} />
-      <button onClick={() => manualUpdateStars(child.id, 1)}>
-        ➕ Add Star
-      </button>
+
       <button
-        onClick={() => manualUpdateStars(child.id, -1)}
-        disabled={child.stars === 0}
+        onClick={() => setShowParentControls((show) => !show)}
+        style={{ padding: "4px", marginTop: "5px" }}
       >
-        ➖ Remove Star
+        {showParentControls
+          ? "Hide Parent Controls ▲"
+          : "Show Parent Controls ▼"}
       </button>
-      <button
-        onClick={() => {
-          const confirmReset = window.confirm(
-            `Are you sure you want to reset all stars for ${child.name}?`
-          );
-          if (confirmReset) {
-            manualUpdateStars(child.id, -child.stars);
-          }
-        }}
-      >
-        {" "}
-        Reset Stars
-      </button>
+
+      {showParentControls && (
+        <div
+          style={{
+            padding: "1rem",
+            borderRadius: "8px",
+          }}
+        >
+          <button onClick={() => manualUpdateStars(child.id, 1)}>
+            ➕ Add Star
+          </button>
+          <button
+            onClick={() => manualUpdateStars(child.id, -1)}
+            disabled={child.stars === 0}
+          >
+            ➖ Remove Star
+          </button>
+          <button
+            onClick={() => {
+              const confirmReset = window.confirm(
+                `Are you sure you want to reset all stars for ${child.name}?`
+              );
+              if (confirmReset) {
+                manualUpdateStars(child.id, -child.stars);
+              }
+            }}
+            disabled={child.stars === 0}
+          >
+            {" "}
+            Reset Stars
+          </button>
+        </div>
+      )}
     </>
   );
 }
