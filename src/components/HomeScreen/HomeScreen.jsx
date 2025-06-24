a1d167d9840400a7150090add2f77fe8e3fb1787
Timothy Li <timothy93li@gmail.com>
2025-06-24 17:21:15 +0100

    fix: prevent slider flicker when sliding between 5 and 10, by fixing label width for totalWords display

--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -6,20 +6,36 @@ export default function HomeScreen({
   totalWords,
   setTotalWords,
 }) {
+  const handleIncrement = () => {
+    if (totalWords < 30) {
+      setTotalWords((prev) => prev + 5);
+    }
+  };
+
+  const handleDecrement = () => {
+    if (totalWords > 5) {
+      setTotalWords((prev) => prev - 5);
+    }
+  };
+
   return (
     <div className="home-screen">
       <p>Hello {childName}, press Start when you are ready.</p>
-      <label>
-        Number of Words: {totalWords}
-        <input
-          type="range"
-          min="5"
-          max="30"
-          step="5"
-          value={totalWords}
-          onChange={(e) => setTotalWords(Number(e.target.value))}
-        ></input>
-      </label>
+      <div>
+        <label>
+          Number of Words: <span className="number-display">{totalWords}</span>
+          <input
+            type="range"
+            min="5"
+            max="30"
+            step="5"
+            value={totalWords}
+            onChange={(e) => setTotalWords(Number(e.target.value))}
+          ></input>
+          <button onClick={handleDecrement}>−</button>
+          <button onClick={handleIncrement}>+</button>
+        </label>
+      </div>
       <button
         onClick={onStart}
         className="start-button"
