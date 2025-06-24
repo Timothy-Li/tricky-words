dc55117c3720a688ce7c94e894bf1fe06b777b0c
Timothy Li <timothy93li@gmail.com>
2025-06-24 21:54:03 +0100

    style: progress bar refactored and styled

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,10 +1,25 @@
+import "./ProgressBar.css";
+
+// export default function ProgressBar({ current, total, score }) {
+//   return (
+//     <div className="progress-wrapper">
+//       <div className="current-progress">
+//         Word {current} out of {total}
+//       </div>
+//       <div classname="total-score">Score: {score}</div>
+//     </div>
+//   );
+// }
+
 export default function ProgressBar({ current, total, score }) {
   return (
-    <div>
-      <div>
-        Word {current} out of {total}
+    <div className="progress-bar">
+      <div className="progress-info">
+        <span className="progress-text">
+          Word {current} / {total}
+        </span>
+        <span className="score-text">⭐ Score: {score}</span>
       </div>
-      <div>Score: {score}</div>
     </div>
   );
 }
